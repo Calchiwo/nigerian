2f792fe678fca9729baa64ea26f1e82ac0290618
Calchiwo <calebwodi33@gmail.com>
2025-07-18 01:17:37 +0100

    Upadated:HTML, CSS & Added JS code

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+console.log('NIGERIAN NETLIFY APP is ready!');
+
+// Add a simple interaction when the page loads
+window.onload = function() {
+    alert('Welcome to NIGERIAN NETLIFY APP!');
+};
