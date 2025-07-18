6836e3937606cce0452a30c8b3672f8f84deac4b
Caleb Wodi <calebwodi33@gmail.com>
2025-07-19 00:59:53 +0100

    Update supabase.js
    removed all API key from supabase.js

--- supabase.js
+++ supabase.js
@@ -1,3 +1,2 @@
 const { createClient } = window['@supabase/supabase-js'];
-const supabase = createClient('https://ifmuqihgfbxyxrxlhjxx.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlmbXVxaWhnZmJ4eXhyeGxoanh4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI4MDA1MTYsImV4cCI6MjA2ODM3NjUxNn0.soSyGRyoR1jDGPW2aJJw8eNtWkLmYVNs-KavBZy4Ib4');
-window.supabase = supabase;
+const supabase = 
